ca8db024cf19b30bf2c3a7601e0ab8fbc4b21b40
Muhammad Ehsan <mehsan.dev@gmail.com>
2023-06-01 10:44:59 -0400

    Configured database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './modules/database/database.module';
+import databaseOptions from './config/db.config';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseOptions],
+      envFilePath: [`.env`],
+    }),
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
   exports: [],
---
+++ src/modules/database/database.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => {
+        return config.get<TypeOrmModuleOptions>('database');
+      },
+    }),
+  ],
+})
+export class DatabaseModule {}
